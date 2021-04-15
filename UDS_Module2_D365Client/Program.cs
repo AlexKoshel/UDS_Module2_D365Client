175f02b762462ba57042992d9ee7f2bdcf24c6a8
Alex <kovsh.07@gmail.com>
2021-04-15 20:43:40 +0300

    add GetRdmCar method

--- UDS_Module2_D365Client/Program.cs
+++ UDS_Module2_D365Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xrm.Tooling.Connector;
+﻿using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Tooling.Connector;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -13,14 +14,18 @@ namespace UDS_Module2_D365Client
     {
         static void Main(string[] args)
         {
-            
-            //string connectionString = ConfigurationManager.ConnectionStrings["MyCRM"].ConnectionString;
             var service = new CrmServiceClient(ConfigurationManager.ConnectionStrings["MyCRM"].ConnectionString);
 
             cr9d3_rent rent = new cr9d3_rent();
-            rent.cr9d3_reserved_pickup = RadmonValues.GetPickUpDate();
-            rent.cr9d3_reserved_handover = RadmonValues.GetHandoverDay(rent.GetAttributeValue<DateTime>("cr9d3_reserved_pickup"));
-            rent.cr9d3_car_class = RadmonValues.GetCarClass(service);
+            rent.cr9d3_reserved_pickup = RandomValues.GetPickUpDate();
+            var pickupDate = rent.cr9d3_reserved_pickup;
+            rent.cr9d3_reserved_handover = RandomValues.GetHandoverDay((DateTime)pickupDate);
+
+            rent.cr9d3_car_class = RandomValues.GetCarClass(service);
+            var carClas = rent.cr9d3_car_class.Name.ToString();
+            var carClasId = rent.cr9d3_car_class.Id.ToString();
+            rent.cr9d3_car = RandomValues.GetRdmCar(service, carClas, carClasId);
+
             service.Create(rent);
 
             Console.WriteLine("Done");
---
+++ UDS_Module2_D365Client/Services/RandomValues.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDS_Module2_D365Client.Services
+{
+    public class RandomValues
+    {
+        public static EntityReference GetCarClass(CrmServiceClient service)
+        {
+            var query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+              <entity name='cr9d3_carclass'>
+                <attribute name='cr9d3_classcode' />
+                <attribute name='createdon' />
+                <attribute name='cr9d3_price' />
+                <attribute name='cr9d3_classdescription' />
+                <attribute name='cr9d3_carclassid' />
+                <order attribute='cr9d3_classcode' descending='false' />
+                <filter type='and'>
+                  <condition attribute='statecode' operator='eq' value='0' />
+                </filter>
+              </entity>
+            </fetch>";
+            var carClass = service.RetrieveMultiple(new FetchExpression(query));
+            var rdmClass = carClass[new Random().Next(0, carClass.Entities.Count)];
+            var Clas = new EntityReference(rdmClass.LogicalName, rdmClass.Id);
+            Clas.Name = rdmClass.GetAttributeValue<string>("cr9d3_classcode"); 
+            return Clas;
+        }
+
+        public static DateTime GetPickUpDate()
+        {
+            Random gen = new Random();
+            var start = new DateTime(2019, 1, 1);
+            var endDate = new DateTime(2020, 12, 31);
+            int range = (endDate - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+
+        public static DateTime GetHandoverDay(DateTime pickUpDate)
+        {
+            Random gen = new Random();
+            double range = gen.Next(1,30);
+            return pickUpDate.AddDays(range);
+        }
+
+        public static EntityReference GetRdmCar(CrmServiceClient service, string clasName, string carClasId)
+        {            
+            var query = $"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+                $"<entity name='cr9d3_car'>" +
+                    $"<attribute name='cr9d3_vinnumber' />" +
+                    $"<attribute name='cr9d3_purchasedate' /> " +
+                    $"<attribute name='cr9d3_productiondate' />" +
+                    $"<attribute name='cr9d3_carmodel' />" +
+                    $"<attribute name='cr9d3_carmanufacturer' />" +
+                    $"<attribute name='cr9d3_carclass' />" +
+                    $"<attribute name='cr9d3_carid' />" +
+                    $"<order attribute='cr9d3_vinnumber' descending='false' />" +
+                    $"<filter type='and'>" +
+                        $"<condition attribute='cr9d3_carclass' operator='eq' uiname='{clasName}' uitype='cr9d3_carclass' value='{carClasId}' />" +
+                    $"</filter>" +
+                $"</entity>" +
+            $"</fetch>";
+            var cars = service.RetrieveMultiple(new FetchExpression(query));
+            var rdmCar = cars[new Random().Next(0, cars.Entities.Count)];
+           return new EntityReference(rdmCar.LogicalName, rdmCar.Id);
+        }
+    }
+}
