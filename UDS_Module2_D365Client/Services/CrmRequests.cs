0b6ea97e1682bd3b81af03aca9df14dc3ccffec2
Alex <kovsh.07@gmail.com>
2021-04-27 18:18:40 +0300

    remove requests to CrmRequests

--- UDS_Module2_D365Client/Services/CrmRequests.cs
+++ UDS_Module2_D365Client/Services/CrmRequests.cs
@@ -1,54 +1,68 @@
 ﻿using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
-using System;
 
 namespace UDS_Module2_D365Client.Services
 {
     public class CrmRequests
     {
-        public static EntityReference GetPickupReport(CrmServiceClient service, string carName, Guid carValue, DateTime requiredDate, string reportType)
+        public static EntityCollection GetCarClassList(CrmServiceClient service)
         {
-            var reportTypeValue = 0;
-            var reportRequiredDate = requiredDate.ToString();
+            var query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+              <entity name='cr9d3_carclass'>
+                <attribute name='cr9d3_classcode' />
+                <attribute name='createdon' />
+                <attribute name='cr9d3_price' />
+                <attribute name='cr9d3_classdescription' />
+                <attribute name='cr9d3_carclassid' />
+                <order attribute='cr9d3_classcode' descending='false' />
+                <filter type='and'>
+                  <condition attribute='statecode' operator='eq' value='0' />
+                </filter>
+              </entity>
+            </fetch>";
 
-            switch (reportType)
-            {
-                case "Return":
-                    reportTypeValue = 970300000;
-                    break;
-                default:
-                    reportTypeValue = 970300001;
-                    break;
-            }            
-                        
-            var query = $"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
-                            $"<entity name='cr9d3_cartransferreport'>" +
-                                $"<attribute name='cr9d3_type' />" +
-                                $"<attribute name='cr9d3_date' />" +
-                                $"<attribute name='cr9d3_damages' />" +
-                                $"<attribute name='cr9d3_damagedescription' />" +
-                                $"<attribute name='cr9d3_car' />" +
-                                $"<attribute name='cr9d3_cartransferreportid' />" +
-                                $"<order attribute='cr9d3_type' descending='false' />" +
-                                    $"<filter type='and'>" +
-                                        $"<condition attribute='cr9d3_car' operator='eq' uiname='{carName}' uitype='cr9d3_car' value='{carValue}' />" +
-                                        $"<condition attribute='cr9d3_type' operator='eq' value='{reportTypeValue}' />" +
-                                    $"</filter>" +
-                            $"</entity>" +
-                        $"</fetch>";
+            return service.RetrieveMultiple(new FetchExpression(query));
+        }
 
-            var requiredReports = service.RetrieveMultiple(new FetchExpression(query));
+        public static EntityCollection GetCarslist(CrmServiceClient service)
+        {
+            var query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                <entity name='cr9d3_car'>
+                <attribute name='cr9d3_vinnumber' />
+                <attribute name='cr9d3_purchasedate' />
+                <attribute name='cr9d3_productiondate' />
+                <attribute name='cr9d3_carmodel' />
+                <attribute name='cr9d3_carmanufacturer' />
+                <attribute name='cr9d3_carclass' />
+                <attribute name='cr9d3_carid' />
+                <order attribute='cr9d3_vinnumber' descending='false' />
+                <filter type='and'>
+                    <condition attribute='statecode' operator='eq' value='0' />
+                </filter>
+                </entity>
+            </fetch>";
 
-            foreach (var report in requiredReports.Entities)
-            {
-                var reportDate = report.GetAttributeValue<DateTime>("cr9d3_date").ToString();
-                if (reportDate == reportRequiredDate)
-                {
-                    return new EntityReference(report.LogicalName, report.Id);
-                }
-            }
-            return null;
+            return service.RetrieveMultiple(new FetchExpression(query));
+        }
+
+        public static EntityCollection GetCustomer(CrmServiceClient service)
+        {
+            var query = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+              <entity name='contact'>
+                <attribute name='fullname' />
+                <attribute name='parentcustomerid' />
+                <attribute name='telephone1' />
+                <attribute name='emailaddress1' />
+                <attribute name='contactid' />
+                <order attribute='fullname' descending='false' />
+                <filter type='and'>
+                  <condition attribute='statecode' operator='eq' value='0' />
+                </filter>
+              </entity>
+            </fetch>";
+
+            return service.RetrieveMultiple(new FetchExpression(query));
         }
     }
 }
